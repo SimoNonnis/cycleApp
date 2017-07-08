e1bd9835668d0d9f95f77de5c0b8b91459d4c989
Simon Nonnis <simononnis@gmail.com>
2017-07-08 13:21:28 +0200

    'Counter'

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
-import {div} from '@cycle/dom'
+import {div, button, p} from '@cycle/dom'
 import xs from 'xstream'
 
-export function App (sources) {
-  const vtree$ = xs.of(
-    div('My Awesome Cycle.js app')
-  )
-  const sinks = {
-    DOM: vtree$
+export function App ({ DOM }) {
+  // INTENT
+  const plus$ = DOM.select('.plus').events('click').mapTo(1);
+  const minus$ = DOM.select('.minus').events('click').mapTo(-1);
+
+  // STATE
+
+  // VDOM
+  return {
+    DOM: xs.of(
+      div([
+        button('.plus', 'Increment'),
+        button('.minus', 'Decrement'),
+        p('0'),
+      ])
+    )
   }
-  return sinks
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import {run} from '@cycle/run'
-import {makeDOMDriver} from '@cycle/dom'
-import {App} from './app'
+import {run} from '@cycle/run';
+import {makeDOMDriver} from '@cycle/dom';
+import {App} from './app';
 
-const main = App
+const main = App;
 
 const drivers = {
   DOM: makeDOMDriver('#app')
-}
+};
 
-run(main, drivers)
+run(main, drivers);
