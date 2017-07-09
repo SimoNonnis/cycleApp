793fc918e092496ff086c1d18a390fdbeb349fb2
Simon Nonnis <simononnis@gmail.com>
2017-07-09 19:05:30 +0200

    'Add @cycle/http'

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import {run} from '@cycle/run';
 import {makeDOMDriver} from '@cycle/dom';
+import {HTTPSource} from '@cycle/http';
 import {App} from './app';
 
 const main = App;
 
 const drivers = {
-  DOM: makeDOMDriver('#app')
+  DOM: makeDOMDriver('#app'),
 };
 
 run(main, drivers);
