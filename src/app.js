1e502483544f4ce0b00ee5d6a8beb5ffd1d61454
Simon Nonnis <simononnis@gmail.com>
2017-07-10 12:06:10 +0200

    'Build request string'

--- src/app.js
+++ src/app.js
@@ -7,9 +7,9 @@ export function App ({ DOM, HTTP }) {
     div([
       h1('GitHub Search'),
       div([
-        labeledField('text', 'Description'),
-        labeledField('text', 'Language'),
-        labeledField('text', 'Stars'),
+        labeledField('Description', '.desc', 'text'),
+        labeledField('Language', '.lang', 'text'),
+        labeledField('Stars', '.stars', 'text'),
       ]),
       hr(),
       div([
@@ -23,9 +23,50 @@ export function App ({ DOM, HTTP }) {
   }
 }
 
-function labeledField(inputType, labelText) {
+// Components
+function labeledField(labelText, selector, inputType) {
   return label([
     p(labelText),
-    input({ attrs: { type: inputType}}),
+    input(selector, { attrs: { type: inputType }}),
   ])
+}
+
+// Intent
+function search(sources) {
+  const category = 'github';
+
+  function inputVal$(selector) {
+    return sources.DOM.select(selector).events('input')
+      .map(e => e.target.value)
+      .startWith('');
+  }
+
+  const searchRequest$ =
+    xs.combine(inputVal$('.desc'), inputVal$('.lang', inputVal$('.stars')))
+      .filter( query => query[0].length > 0)
+      .compose(sources.time.debounce(500))
+      .map(([desc, lang, stars]) => {
+        const tearms = [search];
+
+        if (isNumeric(stars)) {
+          terms.push(`stars:">=${stars}"`);
+        }
+        if (language !== '') {
+          terms.push(`language:"${language}"`);
+        }
+        return {
+          q: terms.join(' ')
+        };
+      })
+      .map(query => ({
+        url: 'https://api.github.com/search/repositories',
+        query,
+        category
+      }))
+      .debug('criteria');
+}
+
+// Helpers
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {run} from '@cycle/run';
 import {makeDOMDriver} from '@cycle/dom';
 import {makeHTTPDriver} from '@cycle/http';
+import {timeDriver} from '@cycle/time';
 import {App} from './app';
 
 const main = App;
@@ -8,6 +9,7 @@ const main = App;
 const drivers = {
   DOM: makeDOMDriver('#app'),
   HTTP: makeHTTPDriver(),
+  time: timeDriver,
 };
 
 run(main, drivers);
